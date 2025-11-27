2a7f5ca390f292bfecabdcf339b7d11851e1efec
Siva <73848686+SivaPaoren@users.noreply.github.com>
2025-11-27 21:33:16 +0700

    initial set up | db configured and redis configured

---
+++ src/main/java/seniorproject/bankifycore/BankifyCoreApplication.java
@@ -0,0 +1,13 @@
+package seniorproject.bankifycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankifyCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankifyCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seniorproject/bankifycore/BankifyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package seniorproject.bankifycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankifyCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
