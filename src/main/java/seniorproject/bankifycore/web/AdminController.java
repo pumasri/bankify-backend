6faba9c9e2e00d20034062e640b37ae3c95eb26d
Siva <73848686+SivaPaoren@users.noreply.github.com>
2026-02-06 16:06:22 +0700

    merged adminController with ClientController

--- src/main/java/seniorproject/bankifycore/web/AdminController.java
+++ src/main/java/seniorproject/bankifycore/web/AdminController.java
@@ -5,10 +5,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import seniorproject.bankifycore.dto.ApproveRotationResponse;
 import seniorproject.bankifycore.dto.RejectRotationResponse;
+import seniorproject.bankifycore.dto.admin.ApproveClientResponse;
 import seniorproject.bankifycore.dto.admin.ResetPinRequest;
+import seniorproject.bankifycore.dto.clientapp.ClientAppResponse;
 import seniorproject.bankifycore.service.AccountService;
 import seniorproject.bankifycore.service.partner.ClientAppService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,18 +28,40 @@ public class AdminController {
         accountService.resetPin(accountId, req);
     }
 
+    //Clients
+    @GetMapping("/clients")
+    @PreAuthorize("hasAnyRole('ADMIN','OPERATOR')")
+    public List<ClientAppResponse> list() {
+        return clientAppService.list();
+    }
 
+    @PatchMapping("/clients/{id}/disable")
+    @PreAuthorize("hasAnyRole('ADMIN','OPERATOR')")
+    public ClientAppResponse disable(@PathVariable UUID id) {
+        return clientAppService.disable(id);
+    }
 
-    @PatchMapping("clients/rotation-requests/{id}/approve")
+    //approve API for client first time creating account
+    @PatchMapping("/clients/{id}/approve")
+    @PreAuthorize("hasAnyRole('ADMIN','OPERATOR')")
+    public ApproveClientResponse approve(@PathVariable UUID id) {
+        return clientAppService.approve(id);
+    }
+
+    //approving api rotation for client
+    @PatchMapping("/clients/rotation-requests/{id}/approve")
     @PreAuthorize("hasAnyRole('ADMIN','OPERATOR')")
     public ApproveRotationResponse approveRotation(@PathVariable UUID id) {
         return clientAppService.approveRotation(id);
     }
 
-    @PatchMapping("clients/rotation-requests/{id}/reject")
+    @PatchMapping("/clients/rotation-requests/{id}/reject")
     @PreAuthorize("hasAnyRole('ADMIN','OPERATOR')")
     public RejectRotationResponse rejectRotation(@PathVariable UUID id) {
         return clientAppService.reject(id);
     }
+
+
+
 
 }
